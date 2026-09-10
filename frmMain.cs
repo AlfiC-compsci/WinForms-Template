43bf2e775677407d17b02bf2462e1b6884014a92
Steven Gilson <249266381+radcliffe-sgilson@users.noreply.github.com>
2026-09-10 15:58:53 +0100

    Retail System Code

---
+++ frmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinForms_Template
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            lvReceipt.View = View.Details;
+            lvReceipt.FullRowSelect = true;
+            lvReceipt.GridLines = true;
+
+            lvReceipt.Columns.Add("Barcode");
+            lvReceipt.Columns.Add("Desc.");
+            lvReceipt.Columns.Add("Unit £");
+            lvReceipt.Columns.Add("Qnty");
+            lvReceipt.Columns.Add("Total");
+
+            ListViewItem row1 = new ListViewItem("105");
+            row1.SubItems.Add("Apples");
+            row1.SubItems.Add("0.60");
+            row1.SubItems.Add("5");
+            row1.SubItems.Add("3.00");
+
+            lvReceipt.Items.Add(row1);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
